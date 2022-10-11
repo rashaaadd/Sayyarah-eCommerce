2585fa4806ecb55458edba6a4b2deddd91201c91
Mohamed Rashad <2017uee1217@mnit.ac.in>
2022-10-11 11:38:08 +0530

    changes on connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db:null
 } 
 module.exports.connect = function(done){
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://rashaaadd:Rashadrm8@uploader.amezgot.mongodb.net/?retryWrites=true&w=majority'
     const dbname = 'sayyarah'
 
     mongoClient.connect(url,(err,data)=>{
