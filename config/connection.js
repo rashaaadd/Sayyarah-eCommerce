780ccd8c1e6544d48b03f1257f8694f629fed97a
Mohamed Rashad <2017uee1217@mnit.ac.in>
2023-01-06 22:56:05 +0530

    Updated view port

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db:null
 } 
 module.exports.connect = function(done){
-    const url = 'mongodb+srv://rashaaadd:Rashadrm8@uploader.amezgot.mongodb.net/?retryWrites=true&w=majority'
+    const url = process.env.MONGODB_CONNECTION_URL
     const dbname = 'sayyarah'
 
     mongoClient.connect(url,(err,data)=>{
